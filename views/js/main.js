75af81eddab582a9c19a20c08a348caab989a359
Sean Mullan <seanmm@mchsi.com>
2016-09-18 17:36:19 -0500

    added will-change style to background pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -522,6 +522,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    elem.style.willChange = 'left';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
